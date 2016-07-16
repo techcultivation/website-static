711d538e06fe3752958d62d602f5fe7b5039c0f6
mo <mo@example>
2016-07-16 08:39:40 +0200

    rsync update is enough, no need to replace all

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 hugo
-rsync -vr -L --progress --rsh="ssh -p 22001" public/* techcultivation-web@web1.techcultivation.org:/var/www/techcultivation/public_html/
-rsync -vr -L --progress --rsh="ssh -p 22001" public/* techcultivation-web@web2.techcultivation.org:/var/www/techcultivation/public_html/
+rsync -av --progress --rsh="ssh -p 22001" public/* techcultivation-web@web1.techcultivation.org:/var/www/techcultivation/public_html/
+rsync -av --progress --rsh="ssh -p 22001" public/* techcultivation-web@web2.techcultivation.org:/var/www/techcultivation/public_html/
