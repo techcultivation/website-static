e289f1b70eb045f6b840d722328d7a4035ea2c62
moba <mo@localhost>
2017-08-05 10:40:59 +0200

    removed second webserver, we only have one at the moment

--- publish.sh
+++ publish.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 hugo
 rsync -av --progress --rsh="ssh -p 22001" public/* techcultivation-web@web1.techcultivation.org:/var/www/techcultivation/public_html/
-rsync -av --progress --rsh="ssh -p 22001" public/* techcultivation-web@web2.techcultivation.org:/var/www/techcultivation/public_html/
